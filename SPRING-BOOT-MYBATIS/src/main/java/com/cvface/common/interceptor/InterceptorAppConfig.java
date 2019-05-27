1525ff535cea95a91cd67defe9363c5558634ad9
LQ55 <2276264677@qq.com>
2019-05-27 11:06:38 +0800

    添加跨域配置项

--- SPRING-BOOT-MYBATIS/src/main/java/com/cvface/common/interceptor/InterceptorAppConfig.java
+++ SPRING-BOOT-MYBATIS/src/main/java/com/cvface/common/interceptor/InterceptorAppConfig.java
@@ -1,20 +1,34 @@
-package com.cvface.common.interceptor;
+package com.bimdata.common.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * 应用程序配置类,作用是将拦截器注册到拦截器注册表ProductServiceInterceptorAppConfig.java
- */
 @Configuration
-public class InterceptorAppConfig implements WebMvcConfigurer{
+public class InterceptorAppConfig implements WebMvcConfigurer {
     @Autowired
     private Interceptor interceptor;
+
+    /**
+     * 配置拦截器
+     * @param registry
+     */
     @Override
-    public void addInterceptors(InterceptorRegistry registry){
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor).addPathPatterns("/**/*");
+    }
+    /**
+     * 重写WebMvcConfigurer的addCorsMappings方法，可以配置跨域的问题
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                .maxAge(3600 * 24);
     }
 }
---
+++ VUE-VUEROUTER-VUEX/README.md
@@ -0,0 +1,45 @@
+# VUE-VUEROUTER-VUEX
+
+### 使用技术
+
+### 项目结构
+```shell
+├── src                                    // 项目源码，包含程序源码和测试源码
+│   ├── api                                // 抽取出的api请求
+│   ├── assets                             // 存放静态资源的文件夹
+│   ├── components                         // 项目组件的文件夹
+│   ├── router                             // 路由
+│   ├—— store                              // 状态管理
+│   ├── test                               // 测试源码存放目录
+├── .gitignore                             // 忽略提交文件配置
+├── package.json                           // package.json
+├── README.md                              // 项目简介与说明
+```
+
+### 启动与安装
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
